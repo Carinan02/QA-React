b2742b011adb75d3787e1282c48b8bee9b01d364
Carinan02 <141795962+Carinan02@users.noreply.github.com>
2025-11-03 06:04:41 +0000

    done with initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from "./Layout"
+import './App.css';
+import LandingForm from "./pages/form/landingForm";
 
+const routes = [{
+  path: "/",
+    element: <Layout />,
+      errorElement: <h1>Page not Found!</h1>,
+        children: [
+      {
+            path: "/",
+            element: <h1>Home Page</h1>
+      },{
+            path:"/audit",
+            element:<h1>Audits Page</h1>
+      },{
+            path:"/report",
+            element:<h1>Reports page</h1>
+      },{
+            path:"/form",
+            element:<LandingForm />
+      },{
+            path:"/dashboard",
+            element: (<><h1 className="text-red-500">Dashboard page</h1></>)
+      }]
+
+      }];
+const router = createBrowserRouter(routes);
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-   <h1 class="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
-  )
+return (<RouterProvider router={router} /> )
 }
-
 export default App
+                                                                                                                        
---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,32 @@
+import { Outlet, NavLink } from "react-router-dom";
+import {
+  Home,
+  NotebookPen,
+  LayoutDashboard,
+  Info,
+  FileText,
+  CircleUser ,
+  LogIn,
+  UserPlus,
+} from "lucide-react"; // modern icon library
+
+export default function Layout() {
+  return (
+    <div className="flex min-h-screen bg-gray-100 text-gray-800">
+        {/*sidebar */}
+        <aside className="w-16 bg-gray-900 text-white flex flex-col items-center py-6 space-y-6">
+            <NavLink to="/" title="Home" className={({isActive}) => `p-2 rouded-xl transition-colors ${isActive ? "bg-yellow-400 text-hray-900" : "hover:bg-gray-700"}`}><Home size={22} /></NavLink>
+            <NavLink to="/form" title="Form" className={({isActive}) => `p-2 rouded-xl transition-colors ${isActive ? "bg-yellow-400 text-hray-900" : "hover:bg-gray-700"}`}><NotebookPen size={22} /></NavLink>
+            <NavLink to="/dashboard" title="Dasboard" className={({isActive}) => `p-2 rouded-xl transition-colors ${isActive ? "bg-yellow-400 text-hray-900" : "hover:bg-gray-700"}`}><LayoutDashboard size={22} /></NavLink>
+            <NavLink to="/audit" title="Dasboard" className={({isActive}) => `p-2 rouded-xl transition-colors ${isActive ? "bg-yellow-400 text-hray-900" : "hover:bg-gray-700"}`}><Info size={22} /></NavLink>
+            <NavLink to="/report" title="Dasboard" className={({isActive}) => `p-2 rouded-xl transition-colors ${isActive ? "bg-yellow-400 text-hray-900" : "hover:bg-gray-700"}`}><FileText size={22} /></NavLink>
+            <div className="flex-grow"></div>
+            <NavLink to="/profile" title="Profile" className={({isActive}) => `p-2 rouded-xl transition-colors ${isActive ? "bg-yellow-400 text-hray-900" : "hover:bg-gray-700"}`}><CircleUser size={22} /></NavLink>
+
+        </aside>
+        <main className="flex-1 p-8">
+          <Outlet />
+        </main>
+    </div>
+  );
+}
